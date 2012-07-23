bbb121657743b067e41c5ab03037548b81b91517
Matt Elliott <melliott@ccistudios.com>
2012-07-23 15:58:06 -0400

    applied lettering.js on first letter of the home pages article

--- template/templates/desena/js/script-init.js
+++ template/templates/desena/js/script-init.js
@@ -4,7 +4,7 @@ window.addEvent('domready', function() {
 	// new CCI.Rollover('li.item-122 img, li.item-105 img', { normal_text: '.', over_text: '_rollover.'});
 
 	// image lettering
-	// $$('h1, h2, h3, h4, h5, h6').lettering('words');
+	$$('.home #comp h1 + p').lettering('letters');
 });
 
 window.addEvent('load', function () {
